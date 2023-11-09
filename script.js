4aa2a53612b6772bad32f6aeb02b8123b3e0ea85
RicardoBachmann <ricardo.bachmann@icloud.com>
2023-11-09 14:38:12 +0100

    Input value is displayed in the console after the btn clicked

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function someFunction() {
-  console.log("test");
-}
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
 
-someFunction();
+const meterToFeet = 3.281;
+const literToGallon = 0.264;
+const kilogramToPound = 2.204;
+
+const unitInputEl = document.getElementById("unit-input");
+const convertBtnEl = document.getElementById("convert-btn");
+const lengthValueEl = document.getElementById("length-value");
+const volumeValueEl = document.getElementById("volume-value");
+const massValueEl = document.getElementById("mass-value");
+
+convertBtnEl.addEventListener("click", function () {
+  console.log(unitInputEl.value);
+});
